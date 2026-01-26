ae1e9b3d03e80c1422fbf49fbad9211aff8b3d0b
ni5arga <git@nisarga.me>
2026-01-26 09:54:05 +0530

    feat: enhance header link styling

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
               rel="noopener noreferrer"
               className="header-link"
             >
-              Docs
+              Source Code & Documentation
             </a>
           </div>
         </div>
